51886bb6619858146ba1ff2aef95a39cb04230a4
Eric Dolch <eric.dolch@gmail.com>
2023-01-24 22:02:55 +0100

    add option to disable keyframe updates

--- bvhio/lib/hierarchy/Joint.py
+++ bvhio/lib/hierarchy/Joint.py
@@ -75,94 +75,107 @@ class Joint(Transform):
                 child.writePose(frame)
         return self
 
-    def attach(self, *nodes: "Joint", keepPosition: bool = False, keepRotation: bool = False, keepScale: bool = False) -> "Joint":
+    def attach(self, *nodes: "Joint", keepPosition: bool = False, keepRotation: bool = False, keepScale: bool = False, updateKeyframes: bool = True) -> "Joint":
         for node in nodes:
             # attach the joint
             super().attach(node, keepPosition=keepPosition, keepRotation=keepRotation, keepScale=keepScale)
 
             # apply attach corrections to animation data
-            for pose in node._Keyframes:
-                if keepPosition: pose.Position = self.SpaceWorldInverse * pose.Position
-                if keepRotation: pose.Rotation = self.RotationWorldInverse * pose.Rotation
-                if keepScale: pose.Scale = self.ScaleWorldInverse * pose.Scale
+            if updateKeyframes:
+                for pose in node._Keyframes:
+                    if keepPosition: pose.Position = self.SpaceWorldInverse * pose.Position
+                    if keepRotation: pose.Rotation = self.RotationWorldInverse * pose.Rotation
+                    if keepScale: pose.Scale = self.ScaleWorldInverse * pose.Scale
         return self
 
-    def detach(self, node: "Joint", keepPosition: bool = False, keepRotation: bool = False, keepScale: bool = False) -> "Joint":
+    def detach(self, node: "Joint", keepPosition: bool = False, keepRotation: bool = False, keepScale: bool = False, updateKeyframes: bool = True) -> "Joint":
 
         # apply detach corrections to animation data
-        for pose in node._Keyframes:
-            if keepPosition: pose.Position = self.SpaceWorld * pose.Position
-            if keepRotation: pose.Rotation = self.RotationWorld * pose.Rotation
-            if keepScale: pose.Scale = self.ScaleWorld * pose.Scale
+        if updateKeyframes:
+            for pose in node._Keyframes:
+                if keepPosition: pose.Position = self.SpaceWorld * pose.Position
+                if keepRotation: pose.Rotation = self.RotationWorld * pose.Rotation
+                if keepScale: pose.Scale = self.ScaleWorld * pose.Scale
 
         # finally detatch the joint
         return super().detach(node, keepPosition, keepRotation, keepScale)
 
-    def clearParent(self, keepPosition: bool = False, keepRotation: bool = False, keepScale: bool = False) -> "Joint":
-        return super().clearParent(keepPosition, keepRotation, keepScale)
+    def clearParent(self, keepPosition: bool = False, keepRotation: bool = False, keepScale: bool = False, updateKeyframes: bool = True) -> "Joint":
+        return super().clearParent(keepPosition, keepRotation, keepScale, updateKeyframes)
 
-    def clearChildren(self, keepPosition: bool = False, keepRotation: bool = False, keepScale: bool = False) -> "Joint":
-        return super().clearChildren(keepPosition, keepRotation, keepScale)
+    def clearChildren(self, keepPosition: bool = False, keepRotation: bool = False, keepScale: bool = False, updateKeyframes: bool = True) -> "Joint":
+        return super().clearChildren(keepPosition, keepRotation, keepScale, updateKeyframes)
 
-    def applyPosition(self, position: glm.vec3 = None, recursive: bool = False) -> "Joint":
+    def applyPosition(self, position: glm.vec3 = None, recursive: bool = False, updateKeyframes: bool = True) -> "Joint":
         # define positional change
         change = -self.PositionLocal if position is None else position
         changeInverse = glm.inverse(self.RotationLocal) * (glm.div(1, self.ScaleLocal) * -change)
 
         # apply to current data
-        super().applyPosition(position, recursive=recursive)
+        recursion = recursive and (not updateKeyframes)
+        super().applyPosition(position, recursive=recursion, updateKeyframes=updateKeyframes)
 
         # apply to keyframes
-        for pose in self._Keyframes:
-            pose.Position += change
-        for child in self._Children:
-            for pose in child._Keyframes:
-                pose.Position += changeInverse
+        if updateKeyframes:
+            for pose in self._Keyframes:
+                pose.Position += change
+            for child in self._Children:
+                for pose in child._Keyframes:
+                    pose.Position += changeInverse
+
+        # may do it recursivly
+        if recursive:
+            for child in self._Children:
+                child.applyPosition(position, recursive=True)
 
         return self
 
-    def applyRotation(self, rotation: glm.quat = None, recursive: bool = False) -> "Joint":
+    def applyRotation(self, rotation: glm.quat = None, recursive: bool = False, updateKeyframes: bool = True) -> "Joint":
         # define rotational change
         change = glm.inverse(self.RotationLocal) if rotation is None else rotation
         changeInverse = glm.inverse(change)
 
         # apply to current data
-        super().applyRotation(rotation, recursive=False)
+        recursion = recursive and (not updateKeyframes)
+        super().applyRotation(rotation, recursive=recursion, updateKeyframes=updateKeyframes)
 
         # apply to keyframes
-        for pose in self._Keyframes:
-            pose.Rotation = change * pose.Rotation
-        for child in self._Children:
-            for pose in child._Keyframes:
-                pose.Position = changeInverse * pose.Position
-                pose.Rotation = changeInverse * pose.Rotation
-            if recursive:
-                child.applyRotation(rotation, recursive=True)
+        if updateKeyframes:
+            for pose in self._Keyframes:
+                pose.Rotation = change * pose.Rotation
+            for child in self._Children:
+                for pose in child._Keyframes:
+                    pose.Position = changeInverse * pose.Position
+                    pose.Rotation = changeInverse * pose.Rotation
+                if recursive:
+                    child.applyRotation(rotation, recursive=True)
 
         return self
 
-    def appyScale(self, scale: glm.vec3 = None, recursive: bool = False) -> "Joint":
+    def appyScale(self, scale: glm.vec3 = None, recursive: bool = False, updateKeyframes: bool = True) -> "Joint":
         # define change in scale
         changeFrame = glm.div(self.ScaleLocal, self._Keyframes[self._CurrentFrame].Scale)
         change = glm.div(1, self.ScaleLocal) if scale is None else scale
         changeInverse = glm.div(1, change)
 
         # apply to current data
-        super().appyScale(scale, recursive=False)
+        recursion = recursive and (not updateKeyframes)
+        super().appyScale(scale, recursive=recursion, updateKeyframes=updateKeyframes)
 
         # apply to keyframes
-        for pose in self._Keyframes:
-            pose.Scale *= changeFrame * change
-        for child in self._Children:
-            for pose in child._Keyframes:
-                pose.Position *= changeInverse
-                pose.Scale *= changeInverse
-            if recursive:
-                child.appyScale(scale, recursive=True)
+        if updateKeyframes:
+            for pose in self._Keyframes:
+                pose.Scale *= changeFrame * change
+            for child in self._Children:
+                for pose in child._Keyframes:
+                    pose.Position *= changeInverse
+                    pose.Scale *= changeInverse
+                if recursive:
+                    child.appyScale(scale, recursive=True)
 
         return self
 
-    def roll(self, degrees: float, recursive: bool = False) -> "Joint":
+    def roll(self, degrees: float, recursive: bool = False, updateKeyframes: bool = True) -> "Joint":
         """Rolls the joint along its local Y axis. Updates the children so there is no spatial change
 
         Returns itself"""
@@ -170,16 +183,19 @@ class Joint(Transform):
         changeInverse = glm.inverse(change)
 
         self.RotationLocal = self.RotationLocal * change
-        for pose in self._Keyframes:
-            pose.Rotation = pose.Rotation * change
+        if updateKeyframes:
+            for pose in self._Keyframes:
+                pose.Rotation = pose.Rotation * change
 
         for child in self._Children:
             child.PositionLocal = changeInverse * child.PositionLocal
             child.RotationLocal = changeInverse * child.RotationLocal
-            for pose in child._Keyframes:
-                pose.Position = changeInverse * pose.Position
-                pose.Rotation = changeInverse * pose.Rotation
-            if recursive: child.roll(degrees, recursive=True)
+            if updateKeyframes:
+                for pose in child._Keyframes:
+                    pose.Position = changeInverse * pose.Position
+                    pose.Rotation = changeInverse * pose.Rotation
+            if recursive:
+                child.roll(degrees, recursive=True)
 
         return self
 
