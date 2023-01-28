1c65f0f0b472e00062cfbf7e88ec070dcece46b2
Eric Dolch <eric.dolch@gmail.com>
2023-01-28 16:51:47 +0100

    refactor parser

--- bvhio/__init__.py
+++ bvhio/__init__.py
@@ -2,3 +2,4 @@ from .lib.shared import Pose
 from .lib.bvh import BvhContainer, RootPose
 from .lib.hierarchy import Joint
 from .lib.Parser import convertBvhToHierarchy, readAsHierarchy, readAsBvh
+from SpatialTransform import Euler
--- bvhio/lib/Parser.py
+++ bvhio/lib/Parser.py
@@ -22,7 +22,7 @@ def parseLine(file: TextIOWrapper, lineNumber: int) -> tuple[int, list[str], tup
 
 
 def readAsBvh(path: str) -> BvhContainer:
-    """Reads .bvh file as simple structure."""
+    """Deserialize .bvh file into a simple structure."""
     if not os.path.exists(path):
         raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
 
@@ -37,7 +37,7 @@ def readAsBvh(path: str) -> BvhContainer:
         line, tokens, debugInfo = parseLine(file, line)
         if tokens[0] != 'ROOT':
             raise SyntaxError('First Joint must be defined as "ROOT"', debugInfo)
-        bvh.Root = parseJoint(file, deserializeJointName(tokens, debugInfo))
+        bvh.Root = _parseJoint(file, _deserializeJointName(tokens, debugInfo))
 
         # check for 'MOTION' start
         line, tokens, debugInfo = parseLine(file, line)
@@ -49,25 +49,25 @@ def readAsBvh(path: str) -> BvhContainer:
         if not tokens[0] == 'Frames:' or not len(tokens) == 2:
             raise SyntaxError('First line of "MOTION" section has to be "Frames: X"', debugInfo)
         else:
-            bvh.FrameCount = deserializeFrameCount(tokens[1:], debugInfo)
+            bvh.FrameCount = _deserializeFrameCount(tokens[1:], debugInfo)
 
         # check for frame rate
         line, tokens, debugInfo = parseLine(file, line)
         if not tokens[0] == 'Frame' or not len(tokens) == 3:
             raise SyntaxError('After frame count must follow "Frame Time"', debugInfo)
         else:
-            bvh.FrameTime = deserializeFrameTime(tokens[2:], debugInfo)
+            bvh.FrameTime = _deserializeFrameTime(tokens[2:], debugInfo)
 
         # parse motion data
         for _ in range(bvh.FrameCount):
             line, tokens, debugInfo = parseLine(file, line)
-            deserializeMotion(bvh.Root, deserializeKeyframe(tokens, debugInfo))
+            _deserializeMotion(bvh.Root, _deserializeKeyframe(tokens, debugInfo))
 
         return bvh
 
 
 def convertBvhToHierarchy(bvhPose: RootPose) -> Joint:
-    """Convers a simple bvh srtucture to a joint hierarchy."""
+    """Convers a deserialized bvh structure into a joint hierarchy."""
     restPose = Pose(bvhPose.Offset, bvhPose.getRotation())
     keyFrames = [(frame, key.copy()) for frame, key in enumerate(bvhPose.Keyframes)]
     joint = Joint(bvhPose.Name, restPose=restPose, keyFrames=keyFrames)
@@ -94,11 +94,11 @@ def convertBvhToHierarchy(bvhPose: RootPose) -> Joint:
 
 
 def readAsHierarchy(path: str) -> Joint:
-    """Reads .bvh file as joint hierarchy."""
+    """Deserialize a .bvh file into a joint hierarchy."""
     return convertBvhToHierarchy(readAsBvh(path).Root).readRestPose(recursive=True)
 
 
-def parseJoint(file: TextIOWrapper, name: str, line: int = 0) -> RootPose:
+def _parseJoint(file: TextIOWrapper, name: str, line: int = 0) -> RootPose:
     # check for open bracket
     line, tokens, debugInfo = parseLine(file, line)
     if tokens[0] != '{' or len(tokens) > 1:
@@ -106,16 +106,16 @@ def parseJoint(file: TextIOWrapper, name: str, line: int = 0) -> RootPose:
 
     # create joint and read channels and offeset
     joint = RootPose(name)
-    joint.Offset = deserializeOffset(file, line)
-    joint.Channels = deserializeChannles(file, line)
+    joint.Offset = _deserializeOffset(file, line)
+    joint.Channels = _deserializeChannles(file, line)
 
     # check for definition end or child joints or end site info
     while (True):
         line, tokens, debugInfo = parseLine(file, line)
         if tokens[0] == 'JOINT':
-            joint.Children.append(parseJoint(file, deserializeJointName(tokens, debugInfo), line))
+            joint.Children.append(_parseJoint(file, _deserializeJointName(tokens, debugInfo), line))
         elif tokens[0] == 'End':
-            joint.EndSite = deserializeEndSite(file, line)
+            joint.EndSite = _deserializeEndSite(file, line)
         elif tokens[0] == '}':
             break
         else:
@@ -125,13 +125,13 @@ def parseJoint(file: TextIOWrapper, name: str, line: int = 0) -> RootPose:
     return joint
 
 
-def deserializeJointName(tokens: list[str], debugInfo: tuple[TextIOWrapper, int, int, str]) -> str:
+def _deserializeJointName(tokens: list[str], debugInfo: tuple[TextIOWrapper, int, int, str]) -> str:
     if not isinstance(tokens, list) and len(tokens) != 2:
         raise SyntaxError('Joint header must be 2-dimensional tuple', debugInfo)
     return tokens[1]
 
 
-def deserializeOffset(file: TextIOWrapper, line: int) -> glm.vec3:
+def _deserializeOffset(file: TextIOWrapper, line: int) -> glm.vec3:
     line, tokens, debugInfo = parseLine(file, line)
     if tokens[0] != 'OFFSET':
         raise SyntaxError('Expected OFFSET definition for joint', debugInfo)
@@ -143,7 +143,7 @@ def deserializeOffset(file: TextIOWrapper, line: int) -> glm.vec3:
         raise SyntaxError('Offset must be numerics only', debugInfo)
 
 
-def deserializeChannles(file: TextIOWrapper, line: int) -> list[str]:
+def _deserializeChannles(file: TextIOWrapper, line: int) -> list[str]:
     line, tokens, debugInfo = parseLine(file, line)
     if tokens[0] != 'CHANNELS':
         raise SyntaxError('Expected CHANNELS definition for joint', debugInfo)
@@ -158,18 +158,18 @@ def deserializeChannles(file: TextIOWrapper, line: int) -> list[str]:
     return tokens[2:]
 
 
-def deserializeEndSite(file: TextIOWrapper, line: int) -> glm.vec3:
+def _deserializeEndSite(file: TextIOWrapper, line: int) -> glm.vec3:
     line, tokens, debugInfo = parseLine(file, line)
     if tokens[0] != '{':
         raise SyntaxError('End Site definition must start with an opening bracket', debugInfo)
-    result = deserializeOffset(file, line)
+    result = _deserializeOffset(file, line)
     line, tokens, debugInfo = parseLine(file, line)
     if tokens[0] != '}':
         raise SyntaxError('End Site definition must start with an opening bracket', debugInfo)
     return result
 
 
-def deserializeFrameTime(data: list, debugInfo: tuple) -> float:
+def _deserializeFrameTime(data: list, debugInfo: tuple) -> float:
     if not isinstance(data, list) or len(data) != 1:
         raise SyntaxError('Frame time must be a 1-dimensional tuple', debugInfo)
     try:
@@ -178,7 +178,7 @@ def deserializeFrameTime(data: list, debugInfo: tuple) -> float:
         raise SyntaxError('Frame time be numerical', debugInfo)
 
 
-def deserializeFrameCount(data: list, debugInfo: tuple) -> float:
+def _deserializeFrameCount(data: list, debugInfo: tuple) -> float:
     if not isinstance(data, list) or len(data) != 1:
         raise SyntaxError('Frame count must be a 1-dimensional tuple', debugInfo)
     try:
@@ -187,14 +187,14 @@ def deserializeFrameCount(data: list, debugInfo: tuple) -> float:
         raise SyntaxError('Frame time be numerical', debugInfo)
 
 
-def deserializeKeyframe(data: list, debugInfo: tuple) -> numpy.ndarray:
+def _deserializeKeyframe(data: list, debugInfo: tuple) -> numpy.ndarray:
     try:
         return numpy.array(list(map(float, data)))
     except ValueError:
         raise SyntaxError('Keyframe must be numerics only', debugInfo)
 
 
-def deserializeMotion(joint: RootPose, data: numpy.ndarray, index=0) -> int:
+def _deserializeMotion(joint: RootPose, data: numpy.ndarray, index=0) -> int:
     position = glm.vec3(joint.Offset)
     rotation = glm.vec3(0)
     rotOrder = ''
@@ -213,5 +213,5 @@ def deserializeMotion(joint: RootPose, data: numpy.ndarray, index=0) -> int:
     joint.Keyframes.append(Pose(position, rotation))
 
     for child in joint.Children:
-        index = deserializeMotion(child, data, index)
+        index = _deserializeMotion(child, data, index)
     return index
