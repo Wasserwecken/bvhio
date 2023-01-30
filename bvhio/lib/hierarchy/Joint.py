02f85f233f5e6c84ac63ba373d4fc7b507a2cf91
Eric Dolch <eric.dolch@gmail.com>
2023-01-30 19:31:45 +0100

    refactor

--- bvhio/lib/hierarchy/Joint.py
+++ bvhio/lib/hierarchy/Joint.py
@@ -8,7 +8,11 @@ class Joint(Transform):
 
     - Bone alignment is expected to be along the Y+ axis.
     - Space is defined as right handed where Y+:up and X+:right and Z-:forward.
-    - Positive rotations are counter clockwise."""
+    - Positive rotations are counter clockwise.
+
+    - The animation is a cualculation of ``Pose = RestPose + Keyframe``
+    - The RestPose and Keyframe data is in local space only.
+    - The method ``readPose()`` combines the RestPose and Keframes."""
 
     @property
     def Parent(self) -> "Joint":
@@ -25,11 +29,11 @@ class Joint(Transform):
 
     @property
     def Keyframes(self) -> list[tuple[int, Pose]]:
-        """Animation data for the joint. A keyframe holds the change of local properties in relation to the rest pose, so that 'Pose = RestPose + Keyframe'.
+        """Animation data for the joint. A keyframe holds the change of local properties in relation to the rest pose, so that ``Pose = RestPose + Keyframe``.
 
     - The first element in the tuple is the frame id and the second element are the local keyframe properties.
-    - This is an ordered list by the frame id. Please do not change this manually, unless you know what your doing.
-    - Negative frame ids cannot exist."""
+    - This is an ordered list by the frame id.
+    - Negative frame ids should not exist."""
         return self._Keyframes
 
     @Keyframes.setter
@@ -60,6 +64,108 @@ class Joint(Transform):
         self._RestPose: Pose = Pose() if restPose is None else restPose
         self._Keyframes: list[tuple[int, Pose]] = [] if keyFrames is None else keyFrames
         self._CurrentFrame = -1
+
+    def getKeyframePose(self, frame: int) -> Pose:
+        """Returns the pose at the given frame id.
+
+        - If the frame number is negative, it will look for the n-th frame from the end.
+        - If there are no keyframes, the joint propetries will not change.
+        - If the frame id is out of the keyframe length, the nearest keyframe propetires are used.
+        - If the frame id is between two keyframes, pose properties are linearly interpolated."""
+        if len(self.Keyframes) == 0: return Pose()
+        if frame < 0: frame = max(0, self.getKeyframeRange(includeChildren=False)[1] + 1 - frame)
+
+        # pose definition
+        index = bisect.bisect_left([key[0] for key in self.Keyframes], frame)
+        if index == len(self.Keyframes):
+            # index is bigger than last frame, take last key
+            return self.Keyframes[-1][1]
+        elif self.Keyframes[index][0] == frame:
+            # index matches a keyframe
+            return self.Keyframes[index][1]
+        else:
+            if index == 0:
+                # index is smaller than first frame, take first key
+                return self.Keyframes[-1][1]
+            else:
+                # index is in between two keyframes, interpolate
+                weight = (self.Keyframes[index][0] + frame) / self.Keyframes[index + 1][0]
+                before = self.Keyframes[index][1]
+                after = self.Keyframes[index + 1][1]
+                return Pose(
+                    glm.lerp(before.Position, after.Position, weight),
+                    glm.lerp(before.Rotation, after.Rotation, weight),
+                    glm.lerp(before.Scale, after.Scale, weight)
+                )
+
+    def insertKeyframePose(self, frame: int, pose: Pose) -> "Joint":
+        """Inserts the given pose to the the keyframes.
+
+        - If there is already a keyframe at the frame id, it will be overwritten.
+        - If the frame number is negative, it counts as the n-th frame from the end.
+        - This pose is added later to the rest pose to calculate the final animation."""
+        if frame < 0: frame = max(0, self.getKeyframeRange(includeChildren=False)[1] + 1 - frame)
+        index = bisect.bisect_left([key[0] for key in self.Keyframes], frame)
+
+        if index == len(self.Keyframes) or self.Keyframes[index][0] != frame:
+            bisect.insort(self.Keyframes, (index, pose))
+        else:
+            self.Keyframes[index][1] = pose
+
+        return self
+
+    def removeKeyframe(self, frame: int, recursive: bool = False) -> "Joint":
+        """Removes the keyframe, if it exists, from the keyframe list.
+
+        - If recursive is True -> Child joints do also load their rest pose.
+
+        Returns itself."""
+        index = bisect.bisect_left([key[0] for key in self.Keyframes], frame)
+
+        if index != len(self.Keyframes) and self.Keyframes[index][0] == frame:
+            self.Keyframes.pop(index)
+
+        if recursive:
+            for child in self.Children:
+                child.readKeyframe(frame=frame, recursive=True)
+
+        return self
+
+    def readKeyframe(self, frame: int, recursive: bool = True) -> "Joint":
+        """Sets the joint properties to the pose data of the keyframe.
+
+        - This is not the final animation data. The animation is calculated as ``Pose = Restpose + Keyframe``
+        - If recursive is True -> Child joints do also load their rest pose.
+
+        Returns itself."""
+        key = self.getKeyframePose(frame=frame)
+        self.Position = key.Position
+        self.Rotation = key.Rotation
+        self.Scale = key.Scale
+
+        if recursive:
+            for child in self.Children:
+                child.readKeyframe(frame=frame, recursive=True)
+
+        return self
+
+    def writeKeyframe(self, frame: int, recursive: bool = True) -> "Joint":
+        """Takes the current local joint properties, and sets this as Keyframe pose.
+
+        - If recursive is True -> Child joints do also load their rest pose.
+
+        Returns itself."""
+        self.insertKeyframePose(frame=frame, pose=Pose(
+            position=self.Position,
+            rotation=self.Rotation,
+            scale=self.Scale
+        ))
+
+        if recursive:
+            for child in self.Children:
+                child.readKeyframe(frame=frame, recursive=True)
+
+        return self
 
     def readRestPose(self, recursive: bool = True) -> "Joint":
         """Sets joint properties to the rest pose.
@@ -124,29 +230,8 @@ class Joint(Transform):
         if len(self.Keyframes) == 0: return self.readRestPose(recursive=False)
         if frame < 0: frame = max(0, self.getKeyframeRange(includeChildren=False)[1] + 1 - frame)
 
-        # pose definition
-        key: Pose = None
-        index = bisect.bisect_left([key[0] for key in self.Keyframes], frame)
-        if index == len(self.Keyframes):
-            # index is bigger than last frame, take last key
-            key = self.Keyframes[-1][1]
-        elif self.Keyframes[index][0] != frame:
-            if index == 0:
-                # index is smaller than first frame, take first key
-                key = self.Keyframes[-1][1]
-            else:
-                # index is in between two keyframes, interpolate
-                weight = (self.Keyframes[index][0] + frame) / self.Keyframes[index + 1][0]
-                before = self.Keyframes[index][1]
-                after = self.Keyframes[index + 1][1]
-                key = Pose(
-                    glm.lerp(before.Position, after.Position, weight),
-                    glm.lerp(before.Rotation, after.Rotation, weight),
-                    glm.lerp(before.Scale, after.Scale, weight)
-                )
-        else:
-            # index mathces a keyframe
-            key = self.Keyframes[index][1]
+        # get animation data
+        key = self.getKeyframePose(frame=frame)
 
         # calculate animation pose
         self._CurrentFrame = frame
@@ -156,32 +241,22 @@ class Joint(Transform):
 
         return self
 
-    def writePose(self, frame: int, recursive: bool = True, updateRestpose: bool = False) -> "Joint":
+    def writePose(self, frame: int, recursive: bool = True) -> "Joint":
         """Sets joint properties as animation pose for the given frame id.
 
+        - If there is already a keyframe at the frame id, it will be overwritten.
         - If the frame number is negative, it counts as the n-th frame from the end.
         - If recursive is True -> Child joints do also write their pose.
-        - If updateRestpose is False -> The rest pose does not change, the keyframe is calculated so that 'Keyframe = Pose - RestPose'.
-        - If updateRestpose is True -> The keyframe does not change, the rest pose is calculated so that 'Restpose = Pose - Keyframe'.
 
         Returns itself."""
-        if frame < 0: frame = max(0, self.getKeyframeRange(includeChildren=False)[1] + 1 - frame)
+        # calculate difference to rest pose
+        self.insertKeyframePose(frame=frame, pose=Pose(
+            position=self.Position - self.RestPose.Position,
+            rotation=glm.inverse(self.RestPose.Rotation) * self.Rotation,
+            scale=self.Scale / self.RestPose.Scale
+        ))
 
-        # binary search, creates key if not exists
-        index = bisect.bisect_left([key[0] for key in self.Keyframes], frame)
-        if index == len(self.Keyframes) or self.Keyframes[index][0] != frame:
-            bisect.insort(self.Keyframes, (index, Pose()))
-        key = self.Keyframes[index][1]
-
-        if updateRestpose:
-            self.RestPose.Position = self.Position - key.Position
-            self.RestPose.Rotation = glm.inverse(key.Rotation) * self.Rotation
-            self.RestPose.Scale = self.Scale / key.Scale
-        else:
-            key.Position = self.Position - self.RestPose.Position
-            key.Rotation = glm.inverse(self.RestPose.Rotation) * self.Rotation
-            key.Scale = self.Scale / self.RestPose.Scale
-
+        # recursion
         if recursive:
             for child in self.Children:
                 child.writePose(frame, recursive=True)
@@ -221,58 +296,6 @@ class Joint(Transform):
 
             if recursive:
                 child.roll(degrees, recursive=True)
-
-        return self
-
-    def applyRestPoseRotationToKeyframes(self, rotation: glm.quat = None, recursive: bool = False) -> "Joint":
-        """Foo
-
-        Returns itself."""
-        # define rotational change
-        change = glm.inverse(self.RestPose.Rotation) if rotation is None else rotation
-        changeInverse = glm.inverse(change)
-
-        # apply changes to itself
-        self.RestPose.Rotation = self.RestPose.Rotation * change
-
-        # apply to keyframes
-        for frame, key in self.Keyframes:
-            key.Position = changeInverse * key.Position
-            key.Rotation = changeInverse * key.Rotation
-
-        # apply changes to children
-        for child in self.Children:
-            child.RestPose.Position = changeInverse * child.RestPose.Position
-            child.RestPose.Rotation = changeInverse * child.RestPose.Rotation
-
-            if recursive:
-                child.applyRestPoseRotationToKeyframes(rotation, recursive)
-
-        return self
-
-    def applyRestPoseScaleToKeyframes(self, scale: glm.vec3 = None, recursive: bool = False) -> "Joint":
-        """Foo
-
-        Returns itself."""
-        # define change in scale
-        change = (1.0 / self.RestPose.Scale) if scale is None else scale
-        changeInverse = (1.0 / change)
-
-        # apply changes to itself
-        self.RestPose.Scale *= change
-
-        # apply to keyframes
-        for frame, key in self.Keyframes:
-            key.Position = changeInverse * key.Position
-            key.Scale = changeInverse * key.Scale
-
-        # keep space for children
-        for child in self.Children:
-            child.RestPose.Position = changeInverse * child.RestPose.Position
-            child.RestPose.Scale = changeInverse * child.RestPose.Scale
-
-            if recursive:
-                child.applyRestPoseScaleToKeyframes(scale, recursive)
 
         return self
 
